a75aee84814168f6cae966bf2a79a26afade240e
matheusjean28 <matheusjean28@gmail.com>
2024-03-04 18:50:49 -0300

    implement search

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,29 @@
-use std::error;
+use std::{
+    error,
+    io::{self, stdin, Read},
+};
+
 
 mod readers;
 fn main() {
-    println!("Hello, world!");
 
-    match readers::reader("Files\\test.txt") {
-        Ok(text) => println!("{}", text),
-        Err(err) => eprintln!("Erro ao ler o arquivo: {}", err),
-    }
+    println!("entry with your text:\n");
+    
+    //gets user input from a comma-separated file 
+    //and saves it to the array
+    //now using fixed string
+    
+    let mut user_input = String::new();
+    io::stdin().read_line(&mut user_input);
+    println!("{}", user_input);
+
+    let mut fields = ["test", "some thing", "like this"];
+    
+
+    readers::create_fields_to_search("\\search.txt",&fields);
+
+    // match readers::reader("Files\\test.txt") {
+    //     Ok(text) => println!("{}", text),
+    //     Err(err) => eprintln!("Erro at read file: {}", err),
+    // }
 }
--- src/readers.rs
+++ src/readers.rs
@@ -1,11 +1,32 @@
 use std::fs;
-use std::io::{self, Read};
+use std::fs::File;
+use std::io::{self, Read, Write};
 
+//creat a temp file if not exist and save fields at them
+pub fn create_fields_to_search(file_path_of_search: &str, array_params: &[&str]) -> io::Result<()> {
+    if !std::path::Path::new("Temp\\temp_list.txt").exists() {
+        
+        //create file if not exist
+        let mut file = File::create("Temp\\temp_list.txt")?;
+
+        //white at file
+        for field in array_params{
+            writeln!(file,"{}", field)?;
+        }
+        
+    } else {
+        //do not
+    };
+    
+    Ok(())
+}
+
+//read file with io stream
 pub fn reader(file_path: &str) -> io::Result<String> {
     let mut file = fs::File::open(file_path)?;
     let mut current_content_string = String::new();
-    
+
     file.read_to_string(&mut current_content_string)?;
-    
+
     Ok(current_content_string)
 }
