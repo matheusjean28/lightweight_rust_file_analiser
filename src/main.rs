7691b165b8f63d2f42064ad6719b30626ccd74c1
matheusjean28 <matheusjean28@gmail.com>
2024-03-05 10:52:42 -0300

    get file path by params

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,28 @@
 use std::{
+    collections::hash_map::RandomState,
     error,
     io::{self, stdin, Read},
 };
 
-
 mod readers;
 fn main() {
-
     println!("entry with your text:\n");
-    
-    //gets user input from a comma-separated file 
+
+    //gets user input from a comma-separated file
     //and saves it to the array
     //now using fixed string
-    
+
     let mut user_input = String::new();
     io::stdin().read_line(&mut user_input);
     println!("{}", user_input);
 
     let mut fields = ["test", "some thing", "like this"];
-    
 
-    readers::create_fields_to_search("\\search.txt",&fields);
+    // let readr = readers::create_fields_to_search(&fields);
+    match readers::create_fields_to_search( "search.txt", &fields) {
+        Ok(text) => println!("{:?}", text),
+        Err(err) => println!("{} :message", err),
+    }
 
     // match readers::reader("Files\\test.txt") {
     //     Ok(text) => println!("{}", text),
--- src/readers.rs
+++ src/readers.rs
@@ -3,25 +3,31 @@ use std::fs::File;
 use std::io::{self, Read, Write};
 
 //creat a temp file if not exist and save fields at them
-pub fn create_fields_to_search(file_path_of_search: &str, array_params: &[&str]) -> io::Result<()> {
+pub fn create_fields_to_search(file_to_read_path: &str, array_params: &[&str]) -> io::Result<()> {
     if !std::path::Path::new("Temp\\temp_list.txt").exists() {
-        
         //create file if not exist
         let mut file = File::create("Temp\\temp_list.txt")?;
 
         //white at file
-        for field in array_params{
-            writeln!(file,"{}", field)?;
+        for field in array_params {
+            println!("{}", field);
+            writeln!(file, "{}", field)?;
         }
-        
     } else {
         //do not
-    };
-    
+    }
+
+    let mut file = File::open(file_to_read_path)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    println!("{}", contents);
+    assert_eq!(contents, "search.txt");
+
     Ok(())
 }
 
 //read file with io stream
+
 pub fn reader(file_path: &str) -> io::Result<String> {
     let mut file = fs::File::open(file_path)?;
     let mut current_content_string = String::new();
