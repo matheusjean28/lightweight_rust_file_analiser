5d2dba31099e494e194c9a5ba3c828fdc95e72b5
matheusjean28 <matheusjean28@gmail.com>
2024-03-11 13:24:14 -0300

    move fn to other component

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,12 @@
-use std::{
-    io::{self, },
-};
+use std::fs:: File;
+use std::io::{self, Read, };
 
-use crate::file_search::create_fields_to_search;
+mod read_file;
 
-
-mod readers;
-mod file_search;
 fn main() {
-    println!("entry with your text:\n");
 
-    //gets user input from a comma-separated file
-    //and saves it to the array
-    //now using fixed string
-
-    // let mut user_input = String::new();
-    // io::stdin().read_line(&mut user_input);
-    // println!("{}", user_input);
-
-    let fields = ["test", "some thing", "like this"];
-
-    //string fixed value
-    let file_path: &str = "Temp\\test.txt";
-
-    // let readr = readers::create_fields_to_search(&fields);
-    let value = file_search::create_fields_to_search(&file_path, &["test", "some thing", "like this"]);
-
-    let result: String = match create_fields_to_search(&file_path, &fields) {
-        Ok(resul) => resul,
-        Err(err) => panic!("not was found: {}", err),
-    };
-    // match readers::reader("Files\\test.txt") {
-    //     Ok(text) => println!("{}", text),
-    //     Err(err) => eprintln!("Erro at read file: {}", err),
-    // }
+        match crate::read_file::read_file_content(&"search.txt"){
+                Ok(content) => println!("{}", content), 
+                Err(err) => eprint!("{err}"),
+        }
 }
---
+++ src/read_file.rs
@@ -0,0 +1,14 @@
+use std::fs:: File;
+use std::io::{self, Read, };
+
+pub fn read_file_content(file_path: &str) -> io::Result<String> {
+    let mut open_file: File = File::open(file_path)?;
+
+
+    let mut buffer_content_string = String::new();
+
+     open_file.read_to_string(&mut buffer_content_string)?;
+
+    Ok(buffer_content_string)
+    
+} 
