ed8f0780c36a312196f60d8262872672d28c3f07
matheusjean28 <matheusjean28@gmail.com>
2024-03-11 14:56:13 -0300

    create file if not exist

---
+++ src/Utils/save_file.rs
@@ -0,0 +1,22 @@
+use std::fs::{self, File};
+use std::io::{self, Write};
+
+pub fn save_file(_content: &String) -> io::Result<File> {
+    let path: String = String::from("Temp\\ temp_list.txt");
+    if !fs::metadata(path.clone()).is_ok()  {
+        println!("file does not exist, creating it!");
+        File::create("Temp\\temp_list.txt")?;
+    }
+
+    let mut file_content = File::create(path)?;
+
+    let mut content = String::new();
+
+    let text_to_write = "teste,teste";
+
+    file_content.write_all(_content.as_bytes())?;
+
+    println!("{:?}", file_content);
+
+    Ok(file_content)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,34 @@
-use std::fs:: File;
-use std::io::{self, Read, };
+use std::fs::File;
+use std::io::{self, Read};
+use std::process;
 
-mod read_file;
-
+mod Utils {
+    pub mod read_file;
+    pub mod save_file; // Remove o caminho absoluto e a extensão do arquivo
+}
+use Utils::{read_file, save_file};
+// mod save_file;
 fn main() {
+    let content: String;
 
-        match crate::read_file::read_file_content(&"search.txt"){
-                Ok(content) => println!("{}", content), 
-                Err(err) => eprint!("{err}"),
+    let content: String = match Utils::read_file::read_file_content(&"search.txt") {
+        Ok(content) => {
+            println!("Was read here: {}", content);
+            content
         }
+        Err(err) => {
+            eprintln!("Err at read file: {}", err);
+            process::exit(1);
+        }
+    };
+
+    match save_file::save_file(&content) {
+        Ok(file) => {
+            println!("File saved: {:?}", file);
+        }
+        Err(err) => {
+            eprintln!("Err at save file: {}", err);
+            process::exit(1);
+        }
+    };
 }
